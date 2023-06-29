b2cf904de74ebc3c83f53c264d9a0bb72210882c
Jeff Shen <shenjeff@princeton.edu>
2023-06-29 16:58:04 -0400

    add make_sgd_step to __init__.py

--- sparsenn/__init__.py
+++ sparsenn/__init__.py
@@ -1,6 +1,7 @@
 from .ad import grad, value_and_grad
 from .filter_ad import filter_grad, filter_value_and_grad
 from .linear import SparseLinear, SparseMLP
+from .opt import make_sgd_step
 
 __all__ = [
     "grad",
@@ -9,4 +10,5 @@ __all__ = [
     "filter_value_and_grad",
     "SparseLinear",
     "SparseMLP",
+    "make_sgd_step",
 ]
