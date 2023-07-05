d00cc6c5a4bc27bfd82885a987cdadec1cfb9c7f
Jeff Shen <shenjeff@princeton.edu>
2023-07-04 20:58:19 -0400

    add utils to init.py

--- sparsenn/__init__.py
+++ sparsenn/__init__.py
@@ -1,16 +1,4 @@
 from .ad import filter_grad, filter_value_and_grad, grad, value_and_grad
 from .linear import ResLinear, ResMLP, SparseLinear, SparseMLP
 from .opt import apply_updates, flatten
-
-__all__ = [
-    "grad",
-    "value_and_grad",
-    "filter_grad",
-    "filter_value_and_grad",
-    "SparseLinear",
-    "SparseMLP",
-    "ResLinear",
-    "ResMLP",
-    "apply_updates",
-    "flatten",
-]
+from .utils import nparams
