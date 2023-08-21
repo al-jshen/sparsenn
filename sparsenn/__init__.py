1eae89765973eb6227846c48aee19807834c85b4
Jeff Shen <shenjeff@princeton.edu>
2023-08-21 14:18:30 -0400

    export chunked_vmap

--- sparsenn/__init__.py
+++ sparsenn/__init__.py
@@ -1,4 +1,4 @@
-from .ad import filter_grad, filter_value_and_grad, grad, value_and_grad
+from .ad import filter_grad, filter_value_and_grad, grad, value_and_grad, chunked_vmap
 from .linear import ResLinear, ResMLP, SparseLinear, SparseMLP
 from .opt import apply_updates, flatten
 from .utils import nparams
